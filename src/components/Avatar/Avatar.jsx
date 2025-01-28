4dffd5e54e288af43ac37dafec2949689a8e2578
DeibyOd <deibyod@gmail.com>
2025-01-28 16:16:24 -0300

    New style

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Avatar.scss';
-import avatar from '../../assets/relojio.png';
+import avatar from '../../assets/arenio.png';
 
 const Avatar = () => {
   return <img className='avatar' src={avatar} alt="ReuniOnTime Avatar" />;
