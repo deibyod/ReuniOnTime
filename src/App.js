41224213e171dbcffcea695aadcd17ccf1118303
DeibyOd <deibyod@gmail.com>
2025-01-30 12:52:07 -0300

    Hotjar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import './App.css';
 import Timer from './components/Timer/Timer';
 import Avatar from './components/Avatar/Avatar';
 import Footer from './components/Footer/Footer';
+import Hotjar from '@hotjar/browser';
+
+const siteId = 5286657;
+const hotjarVersion = 6;
+
+Hotjar.init(siteId, hotjarVersion);
 
 function App() {
   return (
